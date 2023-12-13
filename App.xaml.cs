33ab31bcd7e78e4dfe7860a4619fc9e01e1e10f8
LukaszMichalski03 <118430562+LukaszMichalski03@users.noreply.github.com>
2023-12-13 13:07:45 +0100

    HostUpdate

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
+using TasksProject.EF;
 using TasksProject.Stores;
 using TasksProject.ViewModels;
 
@@ -9,9 +14,16 @@ namespace TasksProject
     /// </summary>
     public partial class App : Application
     {
-        
+        private readonly IHost _host;
+        public App()
+        {
+            _host = Host.CreateDefaultBuilder()
+                .Build();
+            
+        }
         protected override void OnStartup(StartupEventArgs e)
         {
+            _host.Start();
             CurrentUserStore currentUserStore = new CurrentUserStore();
             NavigationStore navigationStore = new NavigationStore();
             navigationStore.CurrentViewModel = new LoginViewModel(navigationStore, currentUserStore);
@@ -21,6 +33,12 @@ namespace TasksProject
             };
             MainWindow.Show();
             base.OnStartup(e);
+        }
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _host.StopAsync();
+            _host.Dispose();
+            base.OnExit(e);
         }
     }
 }
